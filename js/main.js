13ab3f6a599b75a7c4c1f1ebda21d288617cfb42
Andrea Prina <and.prina@gmail.com>
2022-06-23 15:30:24 +0200

    Create grid dinamicamente con js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,11 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
+const CELLS_NUMBER = 100;
+const gridContainer = document.querySelector(".ms_grid-container")
+
+for(let i = 1; i<= CELLS_NUMBER; i++){
+    const gridElement = document.createElement("div");
+    gridElement.classList.add("ms_grid-element");
+    gridContainer.append(gridElement);
+}
